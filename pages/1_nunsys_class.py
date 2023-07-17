d82affe6baefc1289627df5b0a57dc37f034ea2e
Carlos Echeverria <127862732+CarlosPYVLC@users.noreply.github.com>
2023-07-17 12:06:36 +0200

    Add files via upload

--- pages/1_nunsys_class.py
+++ pages/1_nunsys_class.py
@@ -1,29 +1,20 @@
 #libraries and bibliotecas
 import pandas as pd
-import numpy as np
-import re
-import contractions
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.pipeline import Pipeline
-import matplotlib.pyplot as plt
-from haversine import haversine
-import plotly.express as px
-import plotly.graph_objects as go
+#import re
+#import contractions
+#from nltk.corpus import stopwords
+#from nltk.stem import WordNetLemmatizer
+#from nltk.tokenize import word_tokenize
+#from sklearn.feature_extraction.text import CountVectorizer
+#from sklearn.naive_bayes import MultinomialNB
+####from sklearn.model_selection import train_test_split
+###from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+##from sklearn.linear_model import LogisticRegression
+#from sklearn.svm import SVC
+#from sklearn.ensemble import RandomForestClassifier
+#from sklearn.pipeline import Pipeline
 import streamlit as st
 from PIL import Image
-import plotly.express as px
-import folium
-from streamlit_folium import folium_static
 #nltk.download('punkt')
 #nltk.download('stopwords')
 #nltk.download('wordnet')
